7a2757a1c87c84c4bd9c58891d773c663c62984a
Samuel Tindell <sjtindell@gmail.com>
2015-01-03 18:27:03 -0800

    small change

--- gui.py
+++ gui.py
@@ -17,7 +17,7 @@ class MainWindow(QtGui.QWidget):
         self.layout = QtGui.QVBoxLayout(self)
 
         self.run_btn = QtGui.QPushButton('run sim')
-        self.connect(self.run_btn, QtCore.SIGNAL("released()"), self.run)
+        self.connect(self.run_btn, QtCore.SIGNAL("released()"), self.start)
 
         self.plot = pyqtgraph.PlotWidget()
 
@@ -37,7 +37,7 @@ class MainWindow(QtGui.QWidget):
         self.asset.update_price()
         self.plot.plot(self.asset.price_history)
 
-    def run(self):
+    def start(self):
 
         self.plot.clear()
 
